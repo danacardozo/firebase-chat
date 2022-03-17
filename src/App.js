c37b7cab98cb7726bbfabb76e5b9e255d7303b0f
danacardozo <dana.n.cardozo@gmail.com>
2022-03-17 01:45:55 -0300

    3 Login y arreglo de rutas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import './App.css';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
-import React, {useState} from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login"; 
-import Register from './pages/Register';
-import Navbar from './components/NavBar'; 
+import Navbar from "./components/Navbar";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Profile from './pages/Profile'
+import AuthProvider from "./context/auth";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-   
-
-   
   return (
-    <BrowserRouter>
-    <Navbar/>
-    <Switch>
-      <Route exact path="/" component={Home}/>
-    </Switch>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/profile" component={Profile} />
+          <PrivateRoute exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
