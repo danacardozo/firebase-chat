6e8754ffc7c5067844036cb0afabb695e993637c
danacardozo <dana.n.cardozo@gmail.com>
2022-03-18 02:15:06 -0300

    reseteo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import NavBar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Profile from './pages/Profile'
@@ -12,7 +12,7 @@ function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <NavBar />
+        <Navbar />
         <Switch>
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
